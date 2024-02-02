099da0d8718d7f3a8ada0ae6c74deba40f0571ec
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-01 20:10:24 -0500

    Commit_01

---
+++ PROYECTO_2.c
@@ -0,0 +1,178 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+
+
+
+
+void main(){
+
+int op;
+int max=10;
+int pila[max];
+int i=0;
+int opc;
+int a;
+
+
+do{
+printf("\n1. Presentacion\n");
+printf("\n2. Pila\n");
+printf("\n3. Cola \n");
+printf("\n4. Salir\n");
+printf("\n");
+printf("\nSeleccione la opcion que desee (1-4): ");
+scanf("%d",&op);
+printf("\n");
+
+
+switch(op){
+case 1:
+    printf("\nREALIZADO POR: JOY NELATON 8-902-1282\n");
+    printf("\nGRUPO: 1IL-123\n");
+    break;
+case 2:
+    printf("\nPILA\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}while(op!=4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
